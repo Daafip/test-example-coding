d77ecc5d88683609298b80ccb69739370231fc79
Daafip <daafips@gmail.com>
2024-02-29 12:06:52 +0100

    fixed failing test

--- test_example.py
+++ test_example.py
@@ -1,11 +1,11 @@
 from example import add
-from example import substract
+from example import subtract
 
 def test_add():
 	assert add(1,2)==3	
 
 def test_substract():
-	assert substract(2,1)==1 
+	assert subtract(2,1)==1 
 
 def helper():
 	print('works')
