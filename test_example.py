eeb5cb1791cd6ca05f302dff07d2154df9055439
Mariana Villamil Sastre <villamilsastre@astro.rug.nl>
2024-02-29 11:58:31 +0100

    just substract

--- test_example.py
+++ test_example.py
@@ -1,8 +1,8 @@
 from example import add
 from example import substract
 
-#def test_add():
-#	assert add(1,2)==3	
+def test_add():
+	assert add(1,2)==3	
 
 def test_substract():
 	assert substract(2,1)==1 
